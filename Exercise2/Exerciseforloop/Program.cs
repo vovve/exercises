60389cc3a5324f5a416a3e27fe29e3ab17c4d010
vovve <vovve73@icloud.com>
2019-09-24 13:20:28 +0200

    Lösningar till forloop övningarna

---
+++ Exercise2/Exerciseforloop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exerciseforloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning 1
+            //Skriv ett program som en gång loopar igenom talen 1-10 och skriver ut dem på skärmen.
+            /*int[] number = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            for (int i = 0; i < number.Length; i++)
+            {
+            Console.WriteLine (i);
+            }*/
+
+            //Övning 2
+            //Skriv ett program som räknar baklänges från 40-30 och skriver ut dem på skärmen.
+            /*int[] number = new int[] {29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40};
+            for (int i = 40; i > 29; i--)
+            {
+            Console.WriteLine (i);
+            }*/
+        }
+    }
+}
